4b6354a7e108c5f8f964c9d4f10634eef1b4c0e0
NikleshApprobus <97360284+NikleshFernando@users.noreply.github.com>
2024-10-17 00:52:30 +0530

    commit 1

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"v2tqp6VhQ79tRqbMfhfkJSTRioiXU7hls4OjJ6p2qxg=\"\n}"
---
+++ app/login/staff/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1>Staff Dashboard</h1>
+    </div>
+  )
+}
+
+export default page
