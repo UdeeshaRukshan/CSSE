c76c4d21c0e9d26d87df5fd3f260b014b6b78fc2
NikleshApprobus <97360284+NikleshFernando@users.noreply.github.com>
2024-10-19 21:39:25 +0530

    just new

--- .next/types/app/login/staff/dashboard/page.ts
+++ .next/types/app/login/staff/dashboard/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\STZ\OneDrive\Documents\Projects\CSSE\app\login\staff\dashboard\page.tsx
+// File: C:\Users\STZ\OneDrive\Documents\Projects\CSSE\app\login\staff\dashboard\page.jsx
 import * as entry from '../../../../../../app/login/staff/dashboard/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
